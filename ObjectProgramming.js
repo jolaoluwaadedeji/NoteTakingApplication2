0a049563dbbc66e95ea12b78c51120dd56e06cf1
kofo <kofo>
2016-08-17 12:25:40 +0100

    new3

---
+++ ObjectProgramming.js
@@ -0,0 +1,20 @@
+/** Note Application Class**/
+
+
+class NotesApplication{
+		/** class constructor that instatiate an object**/
+
+			constructor(author){
+				this.author=author;
+				const notes=[]
+			}
+		/** function that creates a note list and store all notes as instance property**/
+		
+			create(note_content){
+			
+				
+			}
+			
+		}
+		
+		
