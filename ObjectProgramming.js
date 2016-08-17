f479faa71def72c5fd6793758cb568da901dcd6a
kofo <kofo>
2016-08-17 17:11:16 +0100

    new7

--- ObjectProgramming.js
+++ ObjectProgramming.js
@@ -1,16 +1,14 @@
-/** Note Application Class**/
-
-
+		 /** Class App  **/
 class NotesApplication  {
 		/** class constructor that instatiate an object**/
 
 			constructor (author) {
-				if(typeof author === "string"){
+				if (typeof author === "string") {
 					this.author=author;
 				    this.notes=[]
 				}
-				else{
-					throw new Error('Input not a string')
+				else {
+					alert('Input not a string')
 				    }
 			}
 		/** The create function create a notes list/array to store all the 
@@ -24,16 +22,17 @@ class NotesApplication  {
 		     
 		     	}
 		     	 
-		       else{
-		      		throw new Error('Input not a string')
+		       else {
+		         	alert('Input not a string')
 			       }
 			} 	
         /** The listNotes function lists out each of 
         the notes in the notes list in the following format**/
+
         	listNotes() {
         	   		    
 
-        		for(let i=0; i<this.notes.length; i++){
+        	   for(let i=0; i<this.notes.length; i++) {
   				
   						console.log(`NOTEID: [${i}] \n ${this.notes[i]}\n\n`);
   				   	      
@@ -46,15 +45,20 @@ class NotesApplication  {
         /**The getNote function takes a note_id which refers to the
          index of the note in the notes list and returns the
           content of that note as a string.**/
+
           	getNote(note_id) {
           		
           	  if (typeof(note_id)	=== 'number') {	
-          		return this.notes[note_id]
-          	      }
+
+          		   return this.notes[note_id]
+
+          	       }
           	
           	  else {
-          	 	throw new Error('Input not a number')
-          	      }
+
+          	       alert('Input not a number')
+
+          	       }
           	
           	}
 
@@ -76,33 +80,39 @@ class NotesApplication  {
   			}
   				
   			    else {
-  				throw new Error('Input not a string')	
-  			       }
+  				     alert('Input not a string')	
+  			         }
   			}
 
   		/**The deleteNote function deletes the note at the 
   		index note_id of the notes list**/
-  			deleteNote(note_id){
+  			deleteNote(note_id) {
   			 
-  			 	if (typeof(note_id) === 'number'){
+  			 	if (typeof(note_id) === 'number') {
+
   				return this.notes.splice(note_id,1)
+
   				     }
   				     
   				else {
-  				throw new Error('Input not a number')	
+
+  				     alert('Input not a number')
+
   				     }     
   			}	
 
   		/**The edit function replaces the content in the 
   		note at note_id with new_content**/
-  			 edit(note_id,new_content){
+  			 edit(note_id,new_content) {
   			 	
   			 	if (typeof(note_id) === 'number' && typeof(new_content)	=== 'string') {
-  				this.notes[note_id]=new_content
+  				     this.notes[note_id]=new_content
   			 	       			 }
   			 	
-  			 	else{
-  			 	throw new Error('Give the correct input')	
+  			 	else {
+
+  			 		 alert('Give the correct input')
+
   			 		} 
   				
   			}
