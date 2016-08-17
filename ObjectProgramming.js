0cd1ddc581f31a663c084657a5c9b8c52b279c57
kofo <kofo>
2016-08-17 13:12:57 +0100

    new4

--- ObjectProgramming.js
+++ ObjectProgramming.js
@@ -6,14 +6,20 @@ class NotesApplication{
 
 			constructor(author){
 				this.author=author;
-				const notes=[]
+				let notes=[]
 			}
-		/** function that creates a note list and store all notes as instance property**/
+		/** Create a notes list/array to store all the 
+		notes as an instance property**/
 		
 			create(note_content){
-			
-				
+			   
+				this.notes.push();
 			}
+        /** This function lists out each of 
+        the notes in the notes list in the following format**/
+        	list(){
+        		
+        	}
 			
 		}
 		
