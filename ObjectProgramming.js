29436aef4ac52dae6e866761767c2017428ef553
kofo <kofo>
2016-08-17 16:38:06 +0100

    new6

--- ObjectProgramming.js
+++ ObjectProgramming.js
@@ -1,60 +1,165 @@
 /** Note Application Class**/
 
 
-class NotesApplication{
+class NotesApplication  {
 		/** class constructor that instatiate an object**/
 
-			constructor(author){
-				this.author=author;
-				let notes=[]
+			constructor (author) {
+				if(typeof author === "string"){
+					this.author=author;
+				    this.notes=[]
+				}
+				else{
+					throw new Error('Input not a string')
+				    }
 			}
-		/** Create a notes list/array to store all the 
+		/** The create function create a notes list/array to store all the 
 		notes as an instance property**/
 		
-			create(note_content){
-			   
-				this.notes.push(note_content);
-			}
-        /** This function lists out each of 
-        the notes in the notes list in the following format**/
-        	list(){
-        		for(var i=0;i<this.notes.length;i++){
-        		return "NOTEID:"+i+"  "+this.notes[i]+" "+this.author	
-        		}
-        	}
+			create(note_content) {
 			
-        /**This function takes a note_id which refers to the
+			   if (typeof(note_content)	=== 'string') {
+				    this.notes.push(note_content);
+				    console.log(this.notes);
+		     
+		     	}
+		     	 
+		       else{
+		      		throw new Error('Input not a string')
+			       }
+			} 	
+        /** The listNotes function lists out each of 
+        the notes in the notes list in the following format**/
+        	listNotes() {
+        	   		    
+
+        		for(let i=0; i<this.notes.length; i++){
+  				
+  						console.log(`NOTEID: [${i}] \n ${this.notes[i]}\n\n`);
+  				   	      
+  					
+        		}
+        		console.log(`\nBy Author:${this.author}`);
+        	}
+        	
+			
+        /**The getNote function takes a note_id which refers to the
          index of the note in the notes list and returns the
           content of that note as a string.**/
-          	getNote(note_id){
+          	getNote(note_id) {
+          		
+          	  if (typeof(note_id)	=== 'number') {	
           		return this.notes[note_id]
+          	      }
+          	
+          	  else {
+          	 	throw new Error('Input not a number')
+          	      }
+          	
           	}
 
-        /**This function take a search string, search_text and 
+        /**The search function take a search string, search_text and 
         returns all the notes with that text within it in the following format
         Showing results for search ‘[<search_text>]’**/
   			search(search_text){
-  				for(search_text in this.notes){
-  				return "Showing results for search '[<search_text>]'"+this.notes.indexOf(search_text)+" "+search_text+" "+this.author  	
-  				}
+  		    if (typeof(search_text)	=== 'string') {
+  				for(let i=0; i<this.notes.length; i++){
+  					if(this.notes[i].indexOf(search_text) >= 0){
+  						console.log(`Showing results for search [${search_text}] \n NOTEID: [${i}] \n ${this.notes[i]}\n\n`);
+  				   	      
+  					}
+	  				
+	  			}
+	  			
+	  			//
+	  			console.log(`\nBy Author:${this.author}`);
+  			}
+  				
+  			    else {
+  				throw new Error('Input not a string')	
+  			       }
   			}
 
-  		/**This function deletes the note at the 
+  		/**The deleteNote function deletes the note at the 
   		index note_id of the notes list**/
-  			delete(note_id){
-  				return this.note_id.splice(note_id,1)
+  			deleteNote(note_id){
+  			 
+  			 	if (typeof(note_id) === 'number'){
+  				return this.notes.splice(note_id,1)
+  				     }
+  				     
+  				else {
+  				throw new Error('Input not a number')	
+  				     }     
   			}	
 
-  		/**This function replaces the content in the 
+  		/**The edit function replaces the content in the 
   		note at note_id with new_content**/
-  			edit(note_id,new_content){
-  				if (isNaN(note_id) === false) {
-  				return this.notes[note_id]=new_content
-  				}
-  				else{
-  				return 'no value'	
-  				}
+  			 edit(note_id,new_content){
+  			 	
+  			 	if (typeof(note_id) === 'number' && typeof(new_content)	=== 'string') {
+  				this.notes[note_id]=new_content
+  			 	       			 }
+  			 	
+  			 	else{
+  			 	throw new Error('Give the correct input')	
+  			 		} 
+  				
   			}
-		}
+}
+
+
 		
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
