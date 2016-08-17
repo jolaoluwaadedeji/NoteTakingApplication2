bebbdd69cfded9edf984e4e33cb8bb101390c994
kofo <kofo>
2016-08-17 17:24:34 +0100

    new8

--- ObjectProgramming.js
+++ ObjectProgramming.js
@@ -8,37 +8,43 @@ class NotesApplication  {
 				    this.notes=[]
 				}
 				else {
-					alert('Input not a string')
+
+					alert ('Input not a string')
+				    
 				    }
 			}
 		/** The create function create a notes list/array to store all the 
 		notes as an instance property**/
 		
-			create(note_content) {
+			create (note_content) {
 			
-			   if (typeof(note_content)	=== 'string') {
+			   if (typeof (note_content)	=== 'string') {
 				    this.notes.push(note_content);
-				    console.log(this.notes);
+				    console.log (this.notes);
 		     
 		     	}
 		     	 
 		       else {
-		         	alert('Input not a string')
+
+		         	alert ('Input not a string')
+			       
 			       }
 			} 	
         /** The listNotes function lists out each of 
         the notes in the notes list in the following format**/
 
-        	listNotes() {
+        	listNotes () {
         	   		    
 
-        	   for(let i=0; i<this.notes.length; i++) {
+        	   for (let i=0; i<this.notes.length; i++) {
   				
-  						console.log(`NOTEID: [${i}] \n ${this.notes[i]}\n\n`);
+  						console.log (`NOTEID: [${i}] \n ${this.notes[i]}\n\n`);
   				   	      
   					
-        		}
-        		console.log(`\nBy Author:${this.author}`);
+        			}
+
+        		console.log (`\nBy Author:${this.author}`);
+        	
         	}
         	
 			
@@ -46,9 +52,9 @@ class NotesApplication  {
          index of the note in the notes list and returns the
           content of that note as a string.**/
 
-          	getNote(note_id) {
+          	getNote (note_id) {
           		
-          	  if (typeof(note_id)	=== 'number') {	
+          	  if (typeof (note_id)	=== 'number') {	
 
           		   return this.notes[note_id]
 
@@ -56,7 +62,7 @@ class NotesApplication  {
           	
           	  else {
 
-          	       alert('Input not a number')
+          	       alert ('Input not a number')
 
           	       }
           	
@@ -65,30 +71,32 @@ class NotesApplication  {
         /**The search function take a search string, search_text and 
         returns all the notes with that text within it in the following format
         Showing results for search ‘[<search_text>]’**/
-  			search(search_text){
-  		    if (typeof(search_text)	=== 'string') {
-  				for(let i=0; i<this.notes.length; i++){
-  					if(this.notes[i].indexOf(search_text) >= 0){
-  						console.log(`Showing results for search [${search_text}] \n NOTEID: [${i}] \n ${this.notes[i]}\n\n`);
+  			search (search_text){
+  		    if (typeof (search_text)	=== 'string') {
+  				for (let i=0; i<this.notes.length; i++){
+  					if (this.notes[i].indexOf(search_text) >= 0){
+  						console.log (`Showing results for search [${search_text}] \n NOTEID: [${i}] \n ${this.notes[i]}\n\n`);
   				   	      
   					}
 	  				
-	  			}
+	  			    }
 	  			
 	  			//
 	  			console.log(`\nBy Author:${this.author}`);
-  			}
+  					 }
   				
   			    else {
-  				     alert('Input not a string')	
+
+  				     alert ('Input not a string')	
+  			        
   			         }
   			}
 
   		/**The deleteNote function deletes the note at the 
   		index note_id of the notes list**/
-  			deleteNote(note_id) {
+  			deleteNote (note_id) {
   			 
-  			 	if (typeof(note_id) === 'number') {
+  			 	if (typeof (note_id) === 'number') {
 
   				return this.notes.splice(note_id,1)
 
@@ -96,22 +104,22 @@ class NotesApplication  {
   				     
   				else {
 
-  				     alert('Input not a number')
+  				     alert ('Input not a number')
 
   				     }     
   			}	
 
   		/**The edit function replaces the content in the 
   		note at note_id with new_content**/
-  			 edit(note_id,new_content) {
+  			 edit (note_id,new_content) {
   			 	
-  			 	if (typeof(note_id) === 'number' && typeof(new_content)	=== 'string') {
+  			 	if (typeof (note_id) === 'number' && typeof (new_content)	=== 'string') {
   				     this.notes[note_id]=new_content
-  			 	       			 }
+  			 	    }
   			 	
   			 	else {
 
-  			 		 alert('Give the correct input')
+  			 		 alert ('Give the correct input')
 
   			 		} 
   				
