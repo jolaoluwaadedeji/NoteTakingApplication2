0933398ebe09b7dfead388127a4bd33f73591e7e
kofo <kofo>
2016-08-17 14:35:38 +0100

    new5

--- ObjectProgramming.js
+++ ObjectProgramming.js
@@ -18,11 +18,43 @@ class NotesApplication{
         /** This function lists out each of 
         the notes in the notes list in the following format**/
         	list(){
-        		for(var i=0;i<notes.length;i++){
+        		for(var i=0;i<this.notes.length;i++){
         		return "NOTEID:"+i+"  "+this.notes[i]+" "+this.author,'	
         		}
         	}
 			
+        /**This function takes a note_id which refers to the
+         index of the note in the notes list and returns the
+          content of that note as a string.**/
+          	getNote(note_id){
+          		return this.notes[note_id]
+          	}
+
+        /**This function take a search string, search_text and 
+        returns all the notes with that text within it in the following format
+        Showing results for search ‘[<search_text>]’**/
+  			search(search_text){
+  				for(search_text in this.notes){
+  				return "Showing results for search '[<search_text>]'"+this.notes.indexOf(search_text)+" "+search_text+" "+this.author  	
+  				}
+  			}
+
+  		/**This function deletes the note at the 
+  		index note_id of the notes list**/
+  			delete(note_id){
+  				return this.note_id.splice(note_id,1)
+  			}	
+
+  		/**This function replaces the content in the 
+  		note at note_id with new_content**/
+  			edit(note_id,new_content){
+  				if (isNaN(note_id) === false) {
+  				return this.notes[note_id]=new_content
+  				}
+  				else{
+  				return 'no value'	
+  				}
+  			}
 		}
 		
 		
